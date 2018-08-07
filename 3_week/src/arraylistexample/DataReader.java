4a1ea0d2134796b788b08234971731934e6584e5
Emmet Murray <e.minor.murray@gmail.com>
2018-08-07 11:39:00 +1000

    Added arrayList stuff

---
+++ 3_week/src/arraylistexample/DataReader.java
@@ -0,0 +1,27 @@
+package arraylistexample;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * A class to demonstrate ArrayList usage
+ * 
+ */
+public class DataReader {
+
+	/**
+	 * Reads in ints from stdin and returns all successfully read in ints in an array
+	 */
+	public int[] readData() {
+		
+		// TODO
+		
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		// Bootstrap DataReader
+		// Test it!
+	}
+
+}

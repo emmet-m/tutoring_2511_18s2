015bb90c1ae83d1e4c9e736cf9de3e7f97c7e986
Emmet Murray <e.minor.murray@gmail.com>
2018-08-13 13:15:12 +1000

    Added 4th week

---
+++ 4_week/src/refactoring/solution6/NewReleasePrice.java
@@ -0,0 +1,14 @@
+package refactoring.solution6;
+
+public class NewReleasePrice extends Price {
+
+	@Override
+	public double getCharge(int daysRented){
+		return daysRented * 3;
+	}
+	
+	public int getFrequentRenterPoints(int daysRented) {
+		return (daysRented > 1) ? 2: 1;
+	}
+
+}

86034fd606c743edc1423d120a47850533a01f28
Emmet Murray <e.minor.murray@gmail.com>
2018-08-28 13:57:57 +1000

    Week 6 example

--- 6_week/src/banking/BankAccount.java
+++ 6_week/src/banking/BankAccount.java
@@ -1,13 +1,19 @@
 package banking;
 
+import java.time.LocalDateTime;
+
 /**
  * Class invariant:
- * 
+ *  balance >= 0
+ *  amountWithdrawnToday <= 800
  */
 public class BankAccount {
 	
 	// Invariant: 
 	private float balance;
+	private LocalDateTime today;
+	private float amountWithdrawnToday;
+	
 	
 	//
 	//
@@ -23,10 +29,33 @@ public class BankAccount {
 	 */
 	public BankAccount(float startingBalance) {
 		this.balance = startingBalance;
+		this.today = LocalDateTime.now();
+		this.amountWithdrawnToday = 0;
 	}
 	
-	public boolean withdraw() {
-		// TODO
+	/**
+	 * @pre  amount > 0
+	 * @post true/false if the withdrawal was successful; Class invariant is maintained
+	 * @param amount
+	 * @return
+	 */
+	public boolean withdraw(int amount) {
+		
+		if (this.today.getDayOfMonth() != LocalDateTime.now().getDayOfMonth()) {
+			this.today = LocalDateTime.now();
+		}
+
+		if (!(this.today.getDayOfMonth() == LocalDateTime.now().getDayOfMonth()
+				&& this.amountWithdrawnToday + amount <= 800)) {
+			return false;
+		}
+		
+		if (this.balance - amount >= 0) {
+			this.balance -= amount;
+			return true;
+		}
+		
+
 		return false;
 	}
 	
